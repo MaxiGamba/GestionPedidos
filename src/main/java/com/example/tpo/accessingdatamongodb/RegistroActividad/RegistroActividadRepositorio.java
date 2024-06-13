4e2decadeb9a166bf3309969b85d4fef4387b3d0
facuZ <facundo89je@gmail.com>
2024-06-13 01:37:40 -0300

    bugs

--- src/main/java/com/example/tpo/accessingdatamongodb/RegistroActividad/RegistroActividadRepositorio.java
+++ src/main/java/com/example/tpo/accessingdatamongodb/RegistroActividad/RegistroActividadRepositorio.java
@@ -1,6 +1,5 @@
 package com.example.tpo.accessingdatamongodb.RegistroActividad;
 
-import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
